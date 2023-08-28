232791b0616ff7b3bd2f53398c9673727e53723a
Brian NJoroge <brian.njoroge1@student.moringaschool.com>
2023-08-28 20:35:56 +0300

    completed assignment

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,11 @@
 def write_file(file_name, file_content):
-    pass
+    with open(file_name + ".txt", mode = "w") as file1:
+        file1.write(file_content)
 
 def append_file(file_name, append_content):
-    pass
+    with open(file_name + '.txt', mode = 'a') as file2:
+        file2.write(append_content)
 
 def read_file(file_name):
-    pass
+    with open(file_name + '.txt', mode = 'r') as file3:
+        print(file3.read())
